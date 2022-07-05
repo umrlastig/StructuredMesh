c8cd6d7bc4dc6dfa12653dae6b120774b554ad25
Grégoire Grzeczkowicz <Gregoire.Grzeczkowicz@ign.fr>
2022-07-05 14:52:21 +0000

    Read command line options

---
+++ README.md
@@ -0,0 +1,25 @@
+# Compute LOD2
+Create LOD2 model from DSM, DTM, land use map, LOD0 building footprint and orthophoto.
+
+# Requirements (test version)
+- CGAL (5.4.1)
+- GDAL (3.5.0)
+
+# Compilation
+```bash
+$ mkdir build
+$ cd build
+$ cmake -DCMAKE_BUILD_TYPE=Release ..
+$ make
+```
+
+# Usage
+Usage: `./compute-LOD2` [OPTIONS] -s DSM -t DTM -l land_use_map
+
+OPTIONS:
+- `-h`, `--help`: Print this help anq quit.
+- `-s`, `--DSM=/file/path.tiff`: DSM as TIFF file.
+- `-t`, `--DTM=/file/path.tiff`: DTM as TIFF file.
+- `-l`, `--land_use_map=/file/path.tiff`: land use map as TIFF file.
+- `-0`, `--LOD0=/file/path.shp`: LOD0 as Shapefile.
+- `-i`, `--orthophoto=/file/path.tiff`: RGB orthophoto as TIFF file.
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stdlib.h>
+#include <getopt.h>
+
+int compute_LOD2(char* DSM, char* DTM, char* land_use_map, char* LOD0, char* orthophoto) {
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+	int opt;
+	const struct option options[] = {
+		{"help", no_argument, NULL, 'h'},
+		{"DSM", required_argument, NULL, 's'},
+		{"DTM", required_argument, NULL, 't'},
+		{"land_use_map", required_argument, NULL, 'l'},
+		{"LOD0", required_argument, NULL, '0'},
+		{"orthophoto", required_argument, NULL, 'i'},
+		{NULL, 0, 0, '\0'}
+	};
+
+	char* DSM = NULL;
+	char* DTM = NULL;
+	char* land_use_map = NULL;
+	char* LOD0 = NULL;
+	char* orthophoto = NULL;
+
+	while ((opt = getopt_long(argc, argv, "hs:t:l:0:i:", options, NULL)) != -1) {
+		switch(opt) {
+			case 'h':
+				std::cout << "Usage: " << argv[0] << " [OPTIONS] -s DSM -t DTM -l land_use_map" << std::endl;
+				std::cout << "Build LOD2 representation from DSM, DTM and land use map." << std::endl;
+				std::cout << "OPTIONS:" << std::endl;
+				std::cout << " -h, --help                         Print this help anq quit." << std::endl;
+				std::cout << " -s, --DSM=/file/path.tiff          DSM as TIFF file." << std::endl << std::endl;
+				std::cout << " -t, --DTM=/file/path.tiff          DTM as TIFF file." << std::endl << std::endl;
+				std::cout << " -l, --land_use_map=/file/path.tiff land use map as TIFF file." << std::endl << std::endl;
+				std::cout << " -0, --LOD0=/file/path.shp          LOD0 as Shapefile." << std::endl << std::endl;
+				std::cout << " -i, --orthophoto=/file/path.tiff   RGB orthophoto as TIFF file." << std::endl;
+				return EXIT_SUCCESS;
+				break;
+			case 's':
+				DSM = optarg;
+				break;
+			case 't':
+				DTM = optarg;
+				break;
+			case 'l':
+				land_use_map = optarg;
+				break;
+			case '0':
+				LOD0 = optarg;
+				break;
+			case 'i':
+				orthophoto = optarg;
+				break;
+		}
+	}
+
+	if (DSM == NULL || DTM == NULL || land_use_map == NULL) {
+		std::cerr << "DSM, DTM and land_use_map are mandatory" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " [OPTIONS] -s DSM -t DTM -l land_use_map" << std::endl;
+		std::cerr << "Use -h/--help to obtain more informations" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::cout << "DSM: " << DSM << std::endl;
+	std::cout << "DTM: " << DTM << std::endl;
+	std::cout << "Land use map: " << land_use_map << std::endl;
+	if (LOD0 != NULL) {
+		std::cout << "LOD0: " << LOD0 << std::endl;
+	}
+	if (orthophoto != NULL) {
+		std::cout << "Orthophoto: " << orthophoto << std::endl;
+	}
+	std::cout << std::endl;
+
+	return compute_LOD2(DSM, DTM, land_use_map, LOD0, orthophoto);
+}
