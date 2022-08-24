70826df3d1593abc3d28f23817c3d37025a3ba88
Grégoire Grzeczkowicz <Gregoire.Grzeczkowicz@ign.fr>
2022-08-24 09:30:41 +0000

    Use same color as VRE

--- label.cpp
+++ label.cpp
@@ -14,14 +14,14 @@ struct Label {
 
 std::array<Label, 11> LABELS {
     Label(0, "other", 0, 0, 0),
-    Label(1, "bare ground", 146, 109, 39),
-    Label(2, "low vegetation", 131, 160, 110),
-    Label(3, "water", 40, 67, 96),
-    Label(4, "building", 179, 130, 102),
-    Label(5, "high vegetation", 134, 206, 108),
-    Label(6, "parking", 188, 183, 174),
-    Label(7, "pedestrian", 180, 71, 53),
-    Label(8, "road", 80, 89, 85),
-    Label(9, "railways", 170, 106, 96),
-    Label(10, "swimming pool", 105, 185, 211)
+    Label(1, "bare ground", 100, 50, 0),
+    Label(2, "low vegetation", 0, 250, 50),
+    Label(3, "water", 0, 50, 250),
+    Label(4, "building", 250, 50, 50),
+    Label(5, "high vegetation", 0, 100, 50),
+    Label(6, "parking", 200, 200, 200),
+    Label(7, "pedestrian", 200, 150, 50),
+    Label(8, "road", 100, 100, 100),
+    Label(9, "railways", 200, 100, 200),
+    Label(10, "swimming pool", 50, 150, 250)
 };
